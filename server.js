2d31cf76f67ce9e4075e67eb6fb5189c835cd7c6
jamescodes84 <james.reynolds9@icloud.com>
2024-04-17 17:24:39 -0600

    test

--- server.js
+++ server.js
@@ -160,10 +160,10 @@ router.put('/reviews/', function(req, res) {
     });
   });
   
-  const port = process.env.PORT || 3000;
+ /* const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-  });
+  });*/
 
 
 router.route('/movies')
