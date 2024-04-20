f6810b817a7b83072204d21a71da33b8129a19b5
jamescodes84 <james.reynolds9@icloud.com>
2024-04-20 15:34:50 -0600

    test

--- server.js
+++ server.js
@@ -263,7 +263,7 @@ router.route('/movies')
         const includeReviews = req.query.review === 'true';
 
         if (includeReviews) {
-            res.status(500).json({ message: "Error fetching movie", error: err });
+           /* res.status(500).json({ message: "Error fetching movie", error: err });
             Movie.aggregate([
                 {
                   $match: { _id: movieId } // replace orderId with the actual order id
@@ -286,7 +286,7 @@ router.route('/movies')
                 }
               });
 
-              res.json(result);
+              res.json(result);*/
 
         }
 
