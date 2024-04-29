8f285cca820f848f7f90dccecb565b5c00c2b1fd
jamescodes84 <james.reynolds9@icloud.com>
2024-04-28 18:56:58 -0600

    test

--- server.js
+++ server.js
@@ -286,14 +286,16 @@ router.route('/movies')
     .get(authJwtController.isAuthenticated, (req, res) => {
 
 
-        Movie.find()
-            .then(movies => {
-                res.json(movies);
+        Movie.findById(movieId)
+            .then(movie => {
+                if (!movie) {
+                    return res.status(404).json({ message: "Movie not found" });
+                }
+                res.json(movie);
             })
             .catch(err => {
-                res.status(500).json({ message: "Error fetching movies", error: err });
+                res.status(500).json({ message: "Error fetching movie", error: err });
             });
-    const includeReviews = req.query.reviews;
 
 })
 
